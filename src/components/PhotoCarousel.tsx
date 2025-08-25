551ddb020eb093328507209abef4c1a962a97d01
Johann Gonzales <jgonzalesi@uni.pe>
2025-08-25 13:24:48 -0500

    feat: create arrow button component and change style hover

--- src/components/PhotoCarousel.tsx
+++ src/components/PhotoCarousel.tsx
@@ -1,56 +1,62 @@
 import { useRef, useState } from "react";
-import { PhotoCard } from "./PhotoCard"
-import { MOVEMENT_HORIZONTAL_SCROLL, type MovementHorizontalScrollEnum } from "../lib/Constants";
+import { PhotoCard } from "./PhotoCard";
+import {
+  MOVEMENT_HORIZONTAL_SCROLL,
+  type MovementHorizontalScrollEnum,
+} from "../lib/Constants";
+import { ArrowButton } from "./carousel/ArrowButton";
 
 export const PhotoCarousel: React.FC = () => {
-
   const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
   const scrollRef = useRef<HTMLDivElement>(null);
 
   const scroll = (direction: MovementHorizontalScrollEnum) => {
     const container = scrollRef.current;
-    console.log(`container`, {container})
+    console.log(`container`, { container });
     if (!container) return;
     const scrollAmount = container.offsetWidth * 0.7;
 
-    container.scrollBy(
-      { 
-        left: direction === MOVEMENT_HORIZONTAL_SCROLL.RIGHT ? scrollAmount : -scrollAmount, 
-        behavior: "smooth" 
-      });
+    container.scrollBy({
+      left:
+        direction === MOVEMENT_HORIZONTAL_SCROLL.RIGHT
+          ? scrollAmount
+          : -scrollAmount,
+      behavior: "smooth",
+    });
   };
 
   const imageList = Object.values(
     import.meta.glob(`/src/assets/photos/carrousel/Photo_*.jpg`, {
-      query: '?url',
-      import: 'default',
-      eager: true
+      query: "?url",
+      import: "default",
+      eager: true,
     })
   ) as string[];
 
   return (
-
     <div className="carousel-wrapper">
+      <ArrowButton
+        direction={MOVEMENT_HORIZONTAL_SCROLL.LEFT}
+        onClick={() => scroll(MOVEMENT_HORIZONTAL_SCROLL.LEFT)}
+      />
+      <div className="carousel-container" ref={scrollRef}>
+        {imageList.map((src, idx) => (
+          <PhotoCard
+            key={idx}
+            src={src}
+            alt={`Foto ${idx + 1}`}
+            isHovered={hoveredIndex === idx}
+            isDimmed={hoveredIndex !== null && hoveredIndex !== idx}
+            onHover={() => setHoveredIndex(idx)}
+            onLeave={() => setHoveredIndex(null)}
+          />
+        ))}
+      </div>
 
-     <button className={`arrow arrow-${MOVEMENT_HORIZONTAL_SCROLL.LEFT}`} onClick={() => scroll(MOVEMENT_HORIZONTAL_SCROLL.LEFT)}>‹</button>
-
-    <div className="carousel-container" ref={scrollRef}>
-      {imageList.map((src, idx) => (
-        <PhotoCard 
-        key={idx} src={src} alt={`Foto ${idx + 1}`} 
-        isHovered={hoveredIndex === idx}
-        isDimmed={hoveredIndex !== null && hoveredIndex !== idx}
-        onHover={() => setHoveredIndex(idx)}
-        onLeave={() => setHoveredIndex(null)}
-        />
-      ))}
+      <ArrowButton
+        direction={MOVEMENT_HORIZONTAL_SCROLL.RIGHT}
+        onClick={() => scroll(MOVEMENT_HORIZONTAL_SCROLL.RIGHT)}
+      />
     </div>
-
-     <button className={`arrow arrow-${MOVEMENT_HORIZONTAL_SCROLL.RIGHT}`} onClick={() => scroll(MOVEMENT_HORIZONTAL_SCROLL.RIGHT)}>›</button>
-
-    </div>
-
-    
   );
 };
-
