9d421919fedb53195396bc02b511483eb3a226ca
Johann Gonzales <jgonzalesi@uni.pe>
2025-08-29 00:35:12 -0500

    fix audio sound into backbround

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,7 @@ import './App.css'
 function App() {
 
   return (
-
-    <audio autoPlay={true} loop={true} controls={false} style={{display: 'inline'}}>
-      <source src="/src/assets/music/music.mpeg" type="audio/mpeg" />
-    </audio>
+    <h1></h1>
   )
 }
 
