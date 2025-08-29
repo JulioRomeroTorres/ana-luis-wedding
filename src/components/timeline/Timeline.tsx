5eb8638f432e04c40deedaaae90468c6fe229dd3
Johann Gonzales <jgonzalesi@uni.pe>
2025-08-28 21:07:11 -0500

    add music

--- src/components/timeline/Timeline.tsx
+++ src/components/timeline/Timeline.tsx
@@ -2,32 +2,40 @@ import { HORIZONTAL_ORIENTATION_LIST } from "../../lib/Constants";
 import type { TimelineItemProps } from "../../lib/entities/TimelineItemProps";
 import { TimelineItem } from "./TimelineItem";
 
-export const Timeline = () => {
+export const Timeline: React.FC = () => {
+
+  const photos = Object.values(
+    import.meta.glob(`/src/assets/timeline/photo_*.png`, {
+      query: '?url',
+      import: 'default',
+      eager: true
+    })
+  ) as string[];
   const timeLineItems: TimelineItemProps[] = [
     {
       description: "Misa",
       date: "02:30 P.M.",
-      icon: 'src/assets/timeline/church.png',
+      icon: photos[0],
     },
     {
       description: "Hora Cocktail",
       date: "04:30 P.M.",
-      icon: 'src/assets/timeline/koctail.png'
+      icon: photos[1]
     },
     {
       description: "Entrada de esposos",
       date: "05:30 P.M.",
-      icon: 'src/assets/timeline/rings.png',
+      icon: photos[2]
     },
     {
       description: "Cena",
       date: "06:15 P.M.",
-      icon: 'src/assets/timeline/food.png',
+      icon: photos[3]
     },
     {
       description: "Fiesta",
       date: "07:15 P.M.",
-      icon: 'src/assets/timeline/novios.png',
+      icon: photos[4]
     },
   ];
 
@@ -36,7 +44,7 @@ export const Timeline = () => {
       {timeLineItems.map((item, idx) => {
         const binaryIdx = idx & 1;
         const orientation = HORIZONTAL_ORIENTATION_LIST[binaryIdx];
-        return <TimelineItem key={idx} {...item} orientation={orientation} />;
+        return <TimelineItem key={idx} {...item } icon={photos[idx]} orientation={orientation} />;
       })}
     </div>
   );
